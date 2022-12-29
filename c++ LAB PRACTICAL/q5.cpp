bce2dc22af4212d4c9fb4b554d1806e22645b14f
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:10:56 +0530

    Added

---
+++ c++ LAB PRACTICAL/q5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float fraction,a;
+	int integer;
+	cout<<"enter no in decimal form"<<endl;
+	cin >>a;
+	integer = int(a);
+	fraction= a-integer;
+	cout<<"integer part is "<< integer<< " Fraction Part is "<<fraction<<endl;
+	cout<<"Name = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+    return 0;
+	
+}
