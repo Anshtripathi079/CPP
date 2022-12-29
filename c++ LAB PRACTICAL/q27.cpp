128b5b07cb80deac114d838d24491e885e1ae12c
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:20:23 +0530

    Added

---
+++ c++ LAB PRACTICAL/q27.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class def{
+    public:
+   def(){
+    cout<<"this is default constructor"<<endl;
+   }
+   };
+int main(){
+    def d;    
+	cout << "Name = Ansh Tripathi"<< endl << "Enrollnment no = 07929802021"<<endl;
+}
