7c3fce93bc8dcfd1c366c419ce65d9022a1ed5e8
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:16:21 +0530

    Added

---
+++ c++ LAB PRACTICAL/Q16.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int &b = a;
+    cout<<a<<" "<<b<<endl;
+    ++b;
+    cout<<a<<" "<<b<<endl;
+    cout<<"\nName = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+    }
