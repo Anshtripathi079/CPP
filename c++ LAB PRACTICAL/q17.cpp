83d690bf610b0b8ff2619ea4dbb7b066d2360472
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:16:43 +0530

    Added

---
+++ c++ LAB PRACTICAL/q17.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int a = 5;
+int main(){
+int a =6;
+cout<<"Local variable"<<" "<<a<<endl;
+cout<<"Global variable"<<" "<<::a<<endl;
+cout<<"\nName = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+}
