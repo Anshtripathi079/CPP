1622acbed95a01d0ee498eb6cffd952cf43e732a
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:13:23 +0530

    Added

---
+++ c++ LAB PRACTICAL/q9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	a=9;
+	b=11;
+	b=++a;
+	cout<<"For pre-increment : The value of a is "<<a<<" and b is "<< b<<endl;
+	a=9;
+	b=a++;
+	cout<<"For post-increment : The value of a is "<<a<<" and b is "<< b<<endl;	
+	cout<<"Name = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+    return 0;
+}
