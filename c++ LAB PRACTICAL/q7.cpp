2d8141cb6143344c500f97d9b4421b05ee16d3cb
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:11:58 +0530

    Added

---
+++ c++ LAB PRACTICAL/q7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter number to check for even and odd\n";
+	cin>>n;
+	if(n%2==0)
+		cout<<n<<" is even number"<<endl;
+	else
+		cout<<n<<" is odd number"<<endl;
+	cout<<"Name = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+    return 0;
+}
