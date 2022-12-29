c9a3290ce34c376e09e734719d665ff8a9dca100
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:11:31 +0530

    Added

---
+++ c++ LAB PRACTICAL/q6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,max;
+	cout<<"enter a and b\n";
+	cin>>a>>b;
+	max=(a>b)?a:b;
+	cout<<"The max of two numbers is "<<max<<endl;
+	cout<<"Name = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+    return 0;
+}
