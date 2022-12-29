915099e1148e725d08075d72368e4a5614aac2ce
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:09:36 +0530

    Added

---
+++ c++ LAB PRACTICAL/q2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b ,c;
+	cout << "Enter two numbers to add"<<endl;
+	cin >> a >> b;
+	c = a + b;
+	cout << "Sum = " << c << endl; 
+    cout<<"Name = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+    return 0;
+}
