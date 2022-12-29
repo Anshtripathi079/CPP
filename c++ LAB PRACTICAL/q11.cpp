602282469051eced34b1d364b1171624f1585125
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:14:46 +0530

    Added

---
+++ c++ LAB PRACTICAL/q11.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r;
+	float a,c;
+	cout<<"Enter the radius of a circle\n";
+	cin>>r;
+	a=3.14*r*r;
+	c=2*3.14*r;
+	cout<<"The Area of circle is : "<<a<<endl;
+	cout<<"The Circumference of circle is : "<<c<<endl;
+	cout<<"Name = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+    return 0;
+}
+	
