2e8cde0acd26f36b48385b362a9f334f011ea61a
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:10:18 +0530

    Added

---
+++ c++ LAB PRACTICAL/q3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int s, u, t, a;
+	cout << "enter the values" << endl;
+	cin >> u >> t >> a;
+	s = (u*t)+ (a*(t*t));
+	cout << "Displacement = " << s << endl;
+    cout<<"Name = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+    return 0;
+}
