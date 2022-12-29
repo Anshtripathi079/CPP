2ff9bed62e7be8ede6be0f81b52216a325044fc9
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-12-29 16:14:30 +0530

    Added

---
+++ c++ LAB PRACTICAL/q10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+	cout<<"enter the no. whose you want to find the table\n";
+	cin>>n;
+	for(int i=1;i<11;i++){
+	cout<<n<<" X "<<i<<" = "<<n*i<<endl;
+	}
+	cout<<"Name = Ansh Tripathi"<<endl<<"enrollment no = 07929802021"<<endl;
+}
