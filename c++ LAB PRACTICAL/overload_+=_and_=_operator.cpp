34c48c48c9cf40b18bb5a4f2edd059a822c24422
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-04 21:47:25 +0530

    added overload_+=_and_=_operator

---
+++ c++ LAB PRACTICAL/overload_+=_and_=_operator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class load
+{
+    int a;
+    public:
+    void getdata(int x){
+        a = x;
+    }
+    void showdata(){
+        cout<<a<<endl;
+    }
+    void operator +=(load obj){
+        a = a + obj.a;
+    }
+    void operator =(load obj){
+        a = obj.a;
+    }
+};
+int main(){
+    load o1,o2,o3,o4;
+    o1.getdata(5);
+    o2.getdata(10);
+    o2 += o1;
+    o2.showdata();
+    o3.getdata(3);
+    o4 = o3;
+    o4.showdata();
+}
