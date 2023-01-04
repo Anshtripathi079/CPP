dd85cbb2ecb6d39f4bbecec29049ea0a3137b6d2
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-04 21:38:22 +0530

    added_concat_string_with_overloading

---
+++ c++ LAB PRACTICAL/concate_strings_overloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class concat
+{
+    string s;
+
+public:
+    void getdata(){
+        cin>>s;
+    }
+    void showdata(){
+        cout<<s<<endl;
+    }
+    concat operator +(concat obj){
+        concat t;
+        t.s = s+obj.s;
+        return t;
+    }
+};
+int main(){
+    concat c1,c2,c3;
+    c1.getdata();
+    c2.getdata();
+    c3 = c1+c2;
+    c3.showdata();
+}
