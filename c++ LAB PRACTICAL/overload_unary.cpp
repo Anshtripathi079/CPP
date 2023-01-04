803fbd5bae35a6c4f4e2e0677721948fe8748e7a
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-04 19:00:22 +0530

    Create overload_unary.cpp

---
+++ c++ LAB PRACTICAL/overload_unary.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class load
+{
+    int a;
+    public:
+    void getdata(int x){
+        a = x;
+    }
+    void showdata(){
+        cout<<a<<endl;
+    }
+     void operator ++(){
+         ++a;
+    }
+    void operator ++(int){
+         a++;
+    }
+};
+int main(){
+    load o1;
+    o1.getdata(5);
+    o1.showdata();
+    o1++;
+    o1.showdata();
+    o1++;
+    o1.showdata();
+    ++o1;
+    o1.showdata();
+    cout<<"Ansh Tripathi"<<" "<<"Enrollment no. 079";
+}
+
