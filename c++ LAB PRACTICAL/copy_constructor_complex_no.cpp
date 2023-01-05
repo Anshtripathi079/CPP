6876e602facc4c8846e984a424e814deeb77d545
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-05 16:14:05 +0530

    added copy_constructor_complex_no.cpp

---
+++ c++ LAB PRACTICAL/copy_constructor_complex_no.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+  
+class Complex {
+private:
+    int i, r;
+  
+public:
+    Complex(int x1, int y1)
+    {
+        i = x1;
+        r = y1;
+    }
+ 
+    Complex(const Complex& p1)
+    {
+        i = p1.i;
+        r = p1.r;
+    }
+   void showdata(){
+    cout<<r<<"+i"<<i<<endl;
+   }
+};
+  
+int main()
+{
+    Complex p1(10, 15); 
+    Complex p2 = p1; 
+  
+    p1.showdata();
+    p2.showdata();
+    return 0;
+}
