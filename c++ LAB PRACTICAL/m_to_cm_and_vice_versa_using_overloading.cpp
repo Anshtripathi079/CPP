b6e1764ce63c0e8ebf5931f7046db4e5790f2828
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-05 11:51:38 +0530

    added m_to_cm_and_vice_versa_using_overloading.cpp

---
+++ c++ LAB PRACTICAL/m_to_cm_and_vice_versa_using_overloading.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Meter
+{
+private:
+    float length;
+
+public:
+    Meter()
+    {
+        length = 0.0;
+    }
+    Meter(float Initlength)
+    {
+        length = Initlength / 100.0; // cm to m
+    }
+    operator float()
+    {
+        float Lengthcms;
+        Lengthcms = length * 100.0;
+        return (Lengthcms);
+    }
+    void Getlength()
+    {
+        cout << "\n Enterlength (in meters): ";
+        cin >> length;
+    }
+    void showlength()
+    {
+        cout << "Length (in meter) = " << length;
+    }
+};
+int main()
+{
+    Meter meter1;
+    float length1;
+    cout << "Enter length (in cms): ";
+    cin >> length1;
+    meter1 = length1;
+    meter1.showlength();
+    Meter meter2;
+    float length2;
+    meter2.Getlength();
+    length2 = meter2;
+    cout << "Length (in cms) = " << length2;
+}
