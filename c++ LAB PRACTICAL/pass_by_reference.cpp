b452257ddcd09d48d2958980aa21920961aade15
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-09 12:03:45 +0530

    added pass_by_reference

---
+++ c++ LAB PRACTICAL/pass_by_reference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void swapValues(int &a,int &b){
+int temp = a;
+a = b;
+b = temp;
+}
+int main(){
+    int a=5,b=10;
+    cout<<"before swap "<<endl;
+    cout<<a<<" "<<b<<endl;
+    swapValues(a,b);
+    cout<<"after swap "<<endl;
+    cout<<a<<" "<<b;
+}
