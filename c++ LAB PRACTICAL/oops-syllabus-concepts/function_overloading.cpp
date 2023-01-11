bdd60c1723118578117c3dc5ab9558046dda0b3c
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-11 13:08:47 +0530

    Update function_overloading.cpp

--- c++ LAB PRACTICAL/oops-syllabus-concepts/function_overloading.cpp
+++ c++ LAB PRACTICAL/oops-syllabus-concepts/function_overloading.cpp
@@ -1,3 +1,11 @@
+// Function overloading is a feature of object-oriented programming where two 
+// or more functions can have the same name but different parameters. 
+// When a function name is overloaded with different jobs it is called Function Overloading.
+// In Function Overloading “Function” name should be the same and the arguments should be 
+// different. Function overloading can be considered as an example of a polymorphism 
+// feature in C++.
+
+// function overloading with different no. of arguments.
 #include<iostream>
 using namespace std;
 class A{
@@ -23,3 +31,28 @@ int main()
  obj1.getdata(6,10);
  obj1.showdata();
 }
+
+// function overloading with different type of arguments.
+
+#include <iostream>
+// using namespace std;
+ 
+ 
+// void add(int a, int b)
+// {
+//   cout << "sum = " << (a + b);
+// }
+ 
+// void add(double a, double b)
+// {
+//     cout << endl << "sum = " << (a + b);
+// }
+ 
+// // Driver code
+// int main()
+// {
+//     add(10, 2);
+//     add(5.3, 6.2);
+ 
+//     return 0;
+// }
